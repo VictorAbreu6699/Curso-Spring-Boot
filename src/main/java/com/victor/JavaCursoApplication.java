61a2ea9775a834efc35c240e1b7b09794f2d58c8
Victor Abreu <azevedovictor6699@gmail.com>
2021-04-19 21:09:14 -0300

    Projeto foi Criado

---
+++ src/main/java/com/victor/JavaCursoApplication.java
@@ -0,0 +1,13 @@
+package com.victor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCursoApplication.class, args);
+	}
+
+}
