05e0bffccd86c58f4d76ae025d48abf4a8197b30
Victor Abreu <azevedovictor6699@gmail.com>
2021-04-24 22:28:11 -0300

    Criando operação de Instanciação de objetos para BD

--- src/main/java/com/victor/JavaCursoApplication.java
+++ src/main/java/com/victor/JavaCursoApplication.java
@@ -1,13 +1,31 @@
 package com.victor;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JavaCursoApplication {
+import com.victor.domain.Categoria;
+import com.victor.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class JavaCursoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaCursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
