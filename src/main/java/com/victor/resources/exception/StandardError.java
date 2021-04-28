6e4881ecd7e3ed88f971107a095ee7b6d1686657
Victor Abreu <azevedovictor6699@gmail.com>
2021-04-27 22:58:26 -0300

    Fazendo ajustes no endpoint de categorias

---
+++ src/main/java/com/victor/resources/exception/StandardError.java
@@ -0,0 +1,50 @@
+package com.victor.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer status;
+	private String message;	
+	private long timeStamp;
+		
+	public StandardError(Integer status, String message, long timeStamp) {
+		super();
+		this.status = status;
+		this.message = message;
+		this.timeStamp = timeStamp;
+	}
+	
+	public Integer getStatus() {
+		return status;
+	}
+	
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}	
+	
+}
--- src/main/java/com/victor/services/CategoriaService.java
+++ src/main/java/com/victor/services/CategoriaService.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.victor.domain.Categoria;
 import com.victor.repositories.CategoriaRepository;
+import com.victor.services.exceptions.ObjectNotFoundException;
 
 @Service 
 public class CategoriaService {
@@ -16,8 +18,8 @@ public class CategoriaService {
 	public Categoria find(Integer id)
 	{
 		Optional<Categoria> obj = repo.findById(id);
-		
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException( 
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 	
 	}
 
 }
