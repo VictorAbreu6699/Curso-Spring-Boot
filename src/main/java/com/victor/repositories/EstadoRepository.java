7e2a0a3a1540cb1a254215e10251f8b81ff3b530
Victor Abreu <azevedovictor6699@gmail.com>
2021-05-02 21:59:46 -0300

    Criando Classes Estado e Cidade

---
+++ src/main/java/com/victor/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.victor.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.victor.domain.Estado;
+
+@Repository 
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
