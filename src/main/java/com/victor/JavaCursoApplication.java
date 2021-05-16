dcfde8dedd18dd370ac84156898c979b17a2da4d
Victor Abreu <azevedovictor6699@gmail.com>
2021-05-15 21:39:36 -0300

    Criação das classes ItemPedido e ItemPedidoPK

--- src/main/java/com/victor/JavaCursoApplication.java
+++ src/main/java/com/victor/JavaCursoApplication.java
@@ -13,6 +13,7 @@ import com.victor.domain.Cidade;
 import com.victor.domain.Cliente;
 import com.victor.domain.Endereco;
 import com.victor.domain.Estado;
+import com.victor.domain.ItemPedido;
 import com.victor.domain.Pagamento;
 import com.victor.domain.PagamentoComBoleto;
 import com.victor.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.victor.repositories.CidadeRepository;
 import com.victor.repositories.ClienteRepository;
 import com.victor.repositories.EnderecoRepository;
 import com.victor.repositories.EstadoRepository;
+import com.victor.repositories.ItemPedidoRepository;
 import com.victor.repositories.PagamentoRepository;
 import com.victor.repositories.PedidoRepository;
 import com.victor.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class JavaCursoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaCursoApplication.class, args);
@@ -119,6 +124,19 @@ public class JavaCursoApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.0, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.0, 2, 80.0);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 	}
 
--- src/main/java/com/victor/domain/Produto.java
+++ src/main/java/com/victor/domain/Produto.java
@@ -2,7 +2,10 @@ package com.victor.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -36,11 +40,23 @@ public class Produto implements Serializable {
 	)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto()
 	{
 		id = null;
 		nome = null;
 		preco = null;			
+	}
+	
+	public List<Pedido> getPedidos()
+	{
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido var : itens) {
+			lista.add(var.getPedido());			
+		}
+		return lista;
 	}
 	
 	public Produto(Integer id, String nome, Double preco)
@@ -83,6 +99,14 @@ public class Produto implements Serializable {
 		this.categorias = categorias;
 	}
 	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -107,6 +131,8 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
+
+
 	
 	
 	
