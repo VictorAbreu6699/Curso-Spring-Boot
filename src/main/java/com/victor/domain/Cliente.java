10f902d113f5d01973b70ae4a9cde609bee0637e
Victor Abreu Azevedo <azevedovictor6699@gmail.com>
2021-10-06 18:24:04 -0300

    Salvando perfis de Usuário no banco de dados

--- src/main/java/com/victor/domain/Cliente.java
+++ src/main/java/com/victor/domain/Cliente.java
@@ -5,18 +5,20 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
-import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.victor.domain.enums.Perfil;
 import com.victor.domain.enums.TipoCliente;
 @Entity
 public class Cliente implements Serializable {
@@ -44,9 +46,15 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name="PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
+	
+	
 	public Cliente()
 	{
-		
+		addPerfil(Perfil.CLIENTE);
 	}
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente, String senha) {
 		this.id = id;
@@ -55,6 +63,7 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipoCliente = (tipoCliente==null) ? null : tipoCliente.getCod();	
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 	public Integer getId() {
 		return id;
@@ -127,5 +136,14 @@ public class Cliente implements Serializable {
 		this.pedidos = pedidos;
 	}
 	
+	public Set<Perfil> getPerfis()
+	{
+			return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	
+	public void addPerfil(Perfil perfil)
+	{
+		perfis.add(perfil.getCod());
+	}
 		
 }
